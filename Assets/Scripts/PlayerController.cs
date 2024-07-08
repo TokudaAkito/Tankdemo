acbef2684770f790352862178b0a12cecff675bd
TokudaAkito <ukameiinfu2024128@gmail.com>
2024-07-09 02:36:25 +0900

    弾を腕の先から生成するプログラム作成

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    /// <summary>プレイヤーのrigidbody</summary>
     Rigidbody _prb;
+    /// <summary>プレイヤーの移動速度 </summary>
     [SerializeField] float _moveSpeed;
-
+    /// <summary>弾丸のプレハブ</summary>
+    [SerializeField] GameObject _bullet = null;
+    /// <summary>弾丸の発射位置</summary>
+    [SerializeField] GameObject _leftMuzzle = null;
+    [SerializeField] GameObject _rightMuzzle = null;
     private void Start()
     {
         _prb = GetComponent<Rigidbody>();
@@ -15,6 +21,14 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         Movement();
+        if (Input.GetMouseButton(0))
+        {
+            FireL();
+        }
+        if (Input.GetMouseButton(1))
+        {
+            FireR();
+        }
     }
 
     void Movement()
@@ -34,6 +48,22 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.A))    //左移動
         {
             _prb.velocity = -transform.right * _moveSpeed;
+        }
+    }
+
+    void FireL()
+    {
+        if (_bullet && _leftMuzzle)
+        {
+            GameObject bullets = Instantiate(_bullet, _leftMuzzle.transform.position, _leftMuzzle.transform.rotation);
+        }
+    }
+
+    void FireR()
+    {
+        if (_bullet && _rightMuzzle)
+        {
+            GameObject bullets = Instantiate(_bullet, _rightMuzzle.transform.position, _rightMuzzle.transform.rotation);
         }
     }
 }
